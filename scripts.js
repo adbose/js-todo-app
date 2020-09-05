28dce258eddb8dc359657fe5b013f9f50b5b306c
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-09-06 00:02:23 +0530

    Add render js

---
+++ scripts.js
@@ -0,0 +1,65 @@
+// This is the array that will hold the todo list items
+let todoItems = [];
+
+// render a todo item in the todo items list
+function renderTodo(todo) {
+    // Select the first element with a class of `js-todo-list`
+    const list = document.querySelector('.js-todo-list');
+
+    // Use the ternary operator to check if `todo.checked` is true
+    // if so, assign 'done' to `isChecked`. Otherwise, assign an empty string
+    const isChecked = todo.checked ? 'done': '';
+    
+    // Create an `li` element and assign it to `node`
+    const node = document.createElement("li");
+    // Set the class attribute
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    // Set the data-key attribute to the id of the todo
+    node.setAttribute('data-key', todo.id);
+    // Set the contents of the `li` element created above
+    node.innerHTML = `
+        <input id="${todo.id}" type="checkbox"/>
+        <label for="${todo.id}" class="tick js-tick"></label>
+        <span>${todo.text}</span>
+        <button class="delete-todo js-delete-todo">
+        <svg><use href="#delete-icon"></use></svg>
+        </button>
+    `;
+
+    // Append the element to the DOM as the last child of
+    // the element referenced by the `list` variable
+    list.append(node);
+}
+
+
+// This function will create a new todo object based on the
+// text that was entered in the text input, and push it into
+// the `todoItems` array
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  console.log(todoItems);
+}
+
+// Select the form element
+const form = document.querySelector('.js-form');
+// Add a submit event listener
+form.addEventListener('submit', event => {
+  // prevent page refresh on form submission
+  event.preventDefault();
+  // select the text input
+  const input = document.querySelector('.js-todo-input');
+
+  // Get the value of the input and remove whitespace
+  const texttodoItems = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
