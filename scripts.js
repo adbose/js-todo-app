59547152ae5718196da98b9645a211e23556c4b5
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-09-07 23:37:13 +0530

    Add complete and delete feature

--- scripts.js
+++ scripts.js
@@ -3,32 +3,38 @@ let todoItems = [];
 
 // render a todo item in the todo items list
 function renderTodo(todo) {
-    // Select the first element with a class of `js-todo-list`
-    const list = document.querySelector('.js-todo-list');
+  const list = document.querySelector('.js-todo-list');
+  // select the current todo item in the DOM
+  const item = document.querySelector(`[data-key='${todo.id}']`);
 
-    // Use the ternary operator to check if `todo.checked` is true
-    // if so, assign 'done' to `isChecked`. Otherwise, assign an empty string
-    const isChecked = todo.checked ? 'done': '';
-    
-    // Create an `li` element and assign it to `node`
-    const node = document.createElement("li");
-    // Set the class attribute
-    node.setAttribute('class', `todo-item ${isChecked}`);
-    // Set the data-key attribute to the id of the todo
-    node.setAttribute('data-key', todo.id);
-    // Set the contents of the `li` element created above
-    node.innerHTML = `
-        <input id="${todo.id}" type="checkbox"/>
-        <label for="${todo.id}" class="tick js-tick"></label>
-        <span>${todo.text}</span>
-        <button class="delete-todo js-delete-todo">
-        <svg><use href="#delete-icon"></use></svg>
-        </button>
-    `;
+  // add this if block
+  if (todo.deleted) {
+  // remove the item from the DOM
+  item.remove();
+  return
+  }
 
-    // Append the element to the DOM as the last child of
-    // the element referenced by the `list` variable
+  const isChecked = todo.checked ? 'done': '';
+  const node = document.createElement("li");
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('data-key', todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  // If the item already exists in the DOM
+  if (item) {
+    // replace it
+    list.replaceChild(node, item);
+  } else {
+    // otherwise append it to the end of the list
     list.append(node);
+  }
 }
 
 
@@ -47,6 +53,7 @@ function addTodo(text) {
   renderTodo(todo);
 }
 
+
 // Select the form element
 const form = document.querySelector('.js-form');
 // Add a submit event listener
@@ -64,3 +71,46 @@ form.addEventListener('submit', event => {
     input.focus();
   }
 });
+
+
+// Select the entire list
+const list = document.querySelector('.js-todo-list');
+// Add a click event listener to the list and its children
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+
+function toggleDone(key) {
+  // findIndex is an array method that returns the position of an element
+  // in the array.
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  // Locate the todo item in the todoItems array and set its checked
+  // property to the opposite. That means, `true` will become `false` and vice
+  // versa.
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+
+function deleteTodo(key) {
+  // find the corresponding todo object in the todoItems array
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  // Create a new object with properties of the current todo item
+  // and a `deleted` property which is set to true
+  const todo = {
+    deleted: true,
+    ...todoItems[index]
+  };
+  // remove the todo item from the array by filtering it out
+  todoItems = todoItems.filter(item => item.id !== Number(key));
+  renderTodo(todo);
+}
